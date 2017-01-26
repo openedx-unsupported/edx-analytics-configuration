cbb844ba2e7475365e4e2f2cd6459cc3940e264d
Toby Lawrence <tlawrence@edx.org>
2017-01-26 11:47:50 -0500

    squash! cleanup of unused imports and adding docstrings

--- automation/collect-emr-metrics.py
+++ automation/collect-emr-metrics.py
@@ -1,23 +1,25 @@
 import json
 import requests
-from yarn_api_client import ApplicationMaster, HistoryServer, NodeManager, ResourceManager
+from yarn_api_client import HistoryServer
 
 
 def query_metadata_service(path=''):
   """
-  Queries the instance-local EC2 metadata service, appending the specified path.
+  Gets the value at the specified path by querying the instance metadata serice.
 
-  Returns the textual responses and does not attempt to deserialize the response body.
+  Returns the textual response and does not attempt to deserialize the response body.
   """
-  r = requests.get('http://169.254.169.254/latest/meta-data/' + path)
-  return r.text
+  try:
+    r = requests.get('http://169.254.169.254/latest/meta-data/' + path)
+    return r.text
+  except Exception as e:
+    print "[get-counters] exception when querying instance metadata service for path '{}': {}".format(path, e)
+    return None
 
 
 def get_local_address():
   """
-  Gets the local/private IPv4 address of this instances.
-
-  This assumes the script is being run on an EC2 instance.
+  Gets the local/private IPv4 address of this instance.
   """
   return query_metadata_service('local-ipv4')
 
@@ -31,7 +33,7 @@ def get_instance_id():
 
 def get_job_flow_id():
   """
-  Grabs the job flow ID that this instance belongs to.
+  Gets the job flow ID that this instance belongs to.
   """
   job_flow_id = None
   with open('/mnt/var/lib/info/job-flow.json', 'r') as job_flow_file:
@@ -55,10 +57,25 @@ def get_context():
 
 
 def get_job_name_from_luigi_task_id(task_id):
+  """
+  Gets the name of the job based on the task ID that Luigi uses.
+
+  This is usually in the form of TaskClassName(param=value, param=value, ...) and so we take the simple approach
+  of chopping off whatever is in front of the parameters tuple.
+  """
   return task_id.split('(')[0]
 
 
 def get_prefix_from_counter_group_name(group_name):
+  """
+  Gets the simplified version of a counter group name, suitable for use with Graphite.
+
+  Normally, Hadoop counter groups have names which correspond to the fully qualified Java class name
+  which emitted the counters e.g. org.apache.hadoop.mapreduce.lib.input.FileInputFormatCounter.
+
+  We're simply grabbing the last component in the full string, and then running it through a mapping
+  to generate a simpler version.
+  """
   group_name = group_name.split('.')[-1]
 
   class_name_to_simple_map = {
@@ -89,15 +106,8 @@ def main():
   for (k,v) in context.iteritems():
     print "                 {} => {}".format(k, v)
 
-  # Talk to the ResourceManager to get a list of applications run on the cluster.
-  rm = ResourceManager(local_address)
-  am = ApplicationMaster(local_address)
-  hs = HistoryServer(local_address)
-
-  applications = rm.cluster_applications()
-  print json.dumps(applications.data)
-
   # Grab all jobs from the history server.
+  hs = HistoryServer(local_address)
   response = hs.jobs()
   for job in response.data.get('jobs', {}).get('job', []):
     # Grab details about the job so we get the full name.
@@ -106,17 +116,16 @@ def main():
 
     job_name = get_job_name_from_luigi_task_id(job_info['name'])
     print "[get-counters] Found job '{}' (id: {})".format(job_name, job['id'])
-    #print json.dumps(job_info)
 
     job_counters = hs.job_counters(job['id'])
     job_counters = job_counters.data['jobCounters']
-    #print json.dumps(job_counters)
 
     formatted_job_counters = {}
     for counter_group in job_counters.get('counterGroup', []):
       metric_prefix = get_prefix_from_counter_group_name(counter_group['counterGroupName'])
 
       for metric in counter_group.get('counter', []):
+        # Pull out the counter value for both the map and reduce stages, as well as the total.
         metric_name = metric['name'].lower()
         for type in ['total', 'map', 'reduce']:
           metric_type = type + 'CounterValue'

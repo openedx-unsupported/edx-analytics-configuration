7656560e424de841c1b55fefa0e0338c9d6fe93d
Toby Lawrence <tlawrence@edx.org>
2017-01-30 13:45:41 -0500

    squash! add faceting, cluster name, cleanup

--- automation/collect-emr-metrics.py
+++ automation/collect-emr-metrics.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import boto3
 from yarn_api_client import HistoryServer
 
 
@@ -43,16 +44,28 @@ def get_job_flow_id():
   return job_flow_id
 
 
+def get_cluster_name():
+  """
+  Gets the name of the EMR cluster.
+  """
+  cluster_id = get_job_flow_id()
+
+  emr = boto3.client('emr')
+  cluster = emr.describe_cluster(ClusterId=cluster_id)
+
+  return cluster['Cluster']['Name']
+
+
 def get_context():
   """
   Gets contextual information about where the script is being run.
 
   This includes things like the instance ID, and in the cases of special environments like EMR, the job flow ID.
   """
-
   return {
     'instance_id': get_instance_id(),
-    'job_flow_id': get_job_flow_id()
+    'job_flow_id': get_job_flow_id(),
+    'cluster_name': get_cluster_name(),
   }
 
 
@@ -89,12 +102,49 @@ def get_prefix_from_counter_group_name(group_name):
 
   group_name = class_name_to_simple_map.get(group_name, group_name)
 
+  # Last ditch effort to handle cases not covered here and attempt to pass a viable metric name back.
+  group_name = group_name.replace(' ', '_').lower()
+
   return 'hadoop.counters.' + group_name
 
 
-def main():
+def facet_metrics(metrics, templates, context={}):
+  """
+  Facets the given metrics by creating copies based on the supplied templates.
+
+  This function takes a dictionary of metric name -> metric value mappings, and for each of those metrics,
+  will generate a new metric name (with the same value) by iterating through the list of templates and using
+  both the original metric name and the context dictionary (tags, mostly, like instance ID of job flow ID)
+  to format a new metric name.
+
+  Thus, with a metric of "my.lil.metric", with the tag "foobar" having a value of "quux", and a template of
+  "{foobar}.{metric_name}", we'd get "quux.my.lil.metric".
+  """
+  output_metrics = []
+
+  for metric, data in metrics.iteritems():
+    # Merge our context with any tags for the metric.
+    merged_context = context.copy()
+    merged_context.update(data.get('tags', {}))
+    merged_context['metric_name'] = metric
+
+    # For each "template", feed it in the context so it can be rendered, giving us our transformed metric name.
+    data_value = data.get('value', 0)
+    for template in templates:
+      faceted_metric = template.format(**merged_context)
+      output_metrics.append((faceted_metric, data_value))
+
+  return output_metrics
+
+
+def collect_metrics(metric_templates):
+  """
+  Collects Hadoop counters from all jobs on the local HistoryServer, transforming them for forwarding
+  to Graphite, along with any configured faceting.
+  """
   # Get our local IP (the private IP of the instance) because not all of the YARN services
-  # run on localhost.
+  # run on localhost.  Also, even though this library claims to be able to read the Hadoop
+  # configure files to find out where things are running, it doesn't seem to work fully.
   local_address = get_local_address()
 
   print "[get-counters] Targeting Hadoop on local address '{}'".format(local_address)
@@ -120,7 +170,7 @@ def main():
     job_counters = hs.job_counters(job['id'])
     job_counters = job_counters.data['jobCounters']
 
-    formatted_job_counters = {}
+    formatted_metrics = {}
     for counter_group in job_counters.get('counterGroup', []):
       metric_prefix = get_prefix_from_counter_group_name(counter_group['counterGroupName'])
 
@@ -132,10 +182,22 @@ def main():
           metric_value = metric[metric_type]
           full_metric_path = '.'.join([metric_prefix, metric_name, type])
 
-          formatted_job_counters[full_metric_path] = metric_value
+          formatted_metrics[full_metric_path] = {
+            'value': metric_value,
+            'tags': {
+              'job_name': job_name
+            }
+          }
 
-    print json.dumps(formatted_job_counters)
+
+    return facet_metrics(formatted_metrics, metric_templates, context)
 
 
 if __name__ == "__main__":
-  main()
+  # Define a list of metric name templates, which also us to take in a generated metric,
+  # and generate a new version, or versions, based on the template.
+  metric_templates = [
+    'edx.analytics.emr.{cluster_name}.{job_flow_id}.{instance_id}.{job_name}.{metric}'
+  ]
+
+  print json.dumps(collect_metrics(metric_templates))

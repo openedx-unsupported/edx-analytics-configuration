7aa9841a2fabe01d1f4d30674080b2457bb82670
Hassan Javeed <hassan@edx.org>
2019-02-13 01:41:00 -0500

    Added script to execute shell command on remote cluster.

---
+++ automation/run-shell.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -ex
+
+VENV_ROOT=$WORKSPACE/venvs
+mkdir -p $VENV_ROOT
+
+if [ ! -d "$VENV_ROOT/analytics-tasks" ]
+then
+    virtualenv $VENV_ROOT/analytics-tasks
+fi
+if [ ! -d "$VENV_ROOT/analytics-configuration" ]
+then
+    virtualenv $VENV_ROOT/analytics-configuration
+fi
+
+TASKS_BIN=$VENV_ROOT/analytics-tasks/bin
+CONF_BIN=$VENV_ROOT/analytics-configuration/bin
+
+. $CONF_BIN/activate
+make -C analytics-configuration provision.emr
+
+function terminate_cluster() {
+    . $CONF_BIN/activate
+    make -C analytics-configuration terminate.emr
+}
+if [ "$TERMINATE" = "true" ]; then
+    trap terminate_cluster EXIT
+fi
+
+. $TASKS_BIN/activate
+make -C analytics-tasks bootstrap
+
+remote-task --job-flow-name="$CLUSTER_NAME" --user $TASK_USER --sudo-user '' --shell "$@"
+
